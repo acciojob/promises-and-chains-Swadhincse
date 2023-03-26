f1d68731f9607e0f4e7e134dc4c7702b77c084b9
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-03-27 00:05:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+function submitForm() {
+  const ageInput = document.getElementById('age');
+  const nameInput = document.getElementById('name');
+
+  if (!ageInput.value || !nameInput.value) {
+    alert('Please fill out all fields.');
+    return;
+  }
+
+  const age = Number(ageInput.value);
+  const name = nameInput.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve({ age, name });
+      } else {
+        reject({ age, name });
+      }
+    }, 4000);
+  });
+
+  promise
+    .then(({ age, name }) => {
+      alert(`Welcome, ${name}. You can vote.`);
+      return age;
+    })
+    .then((age) => {
+      return { age };
+    })
+    .then((data) => {
+      alert(`Final object: ${JSON.stringify(data)}`);
+    })
+    .catch(({ age, name }) => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  submitForm();
+});
